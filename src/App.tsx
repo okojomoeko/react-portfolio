37a97be3e8900d5ffa7df1d4a7163b614cade204
okojomoeko <chikoro.size@gmail.com>
2022-01-05 10:22:20 +0000

    add jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './App.css';
 
-export default function App() {
-  return <div>Hello World!</div>
+// import About from './components/About';
+// import { NavBar } from './components/NavBar';
+// import Works from './components/Works';
+// import SkillsAndInterests from './components/SkillsAndInterests';
+// import HeroSection from './components/Hero';
+
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+function App() {
+  const [themeFlagState, setthemeFlagState] = React.useState({
+    themeFlag: true,
+  });
+
+  // TODO: for dark mode
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: themeFlagState.themeFlag ? 'dark' : 'light',
+        },
+      }),
+    [themeFlagState]
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className='App'>
+        {/* <NavBar themeFlagState={themeFlagState} setState={setthemeFlagState} />
+        <HeroSection />
+        <About />
+        <SkillsAndInterests />
+        <Works /> */}
+      </div>
+    </ThemeProvider>
+  );
 }
+
+export default App;
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import App from '../App';
+import { intersectionObserverMock } from '../__mocks__/intersectionObserverMock';
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+});
+
+test('renders learn react link', () => {
+  const { container, getAllByRole } = render(<App />);
+
+  // const buttons = getAllByRole('button');
+
+  // const headerButtons = buttons.slice(0, 3);
+
+  // const expectHeadderButtons = ['ABOUT', 'SKILLS', 'WORKS'];
+
+  // let count = 0;
+  // for (let button of headerButtons) {
+  //   expect(button.outerHTML).toContain(expectHeadderButtons[count]);
+  //   count++;
+  // }
+  expect(container.innerHTML).toMatchSnapshot();
+});
